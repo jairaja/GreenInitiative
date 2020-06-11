f9646ef24f1c4d7bcedf35ab35391d74789f13f6
Rajat <rajatjain.com@gmail.com>
2020-06-11 08:48:14 +0530

    incremental

--- src/vehicle-owner/find-rider.tsx
+++ src/vehicle-owner/find-rider.tsx
@@ -1,19 +1,58 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { ButtonGroup } from 'react-native-elements';
+import config from '../common/config';
 
 interface FindRiderProp {
   visible: boolean;
 }
 
-export default function FindRider(prop: FindRiderProp) {
+function FindRider(prop: FindRiderProp) {
+
+  const [findRiderState, setFindRiderState] = useState({
+    selectedRouteIndex: 0,
+  });
+
   return (
     <>
       {
         prop.visible &&
-        <View>
-          <Text>find rider</Text>
+        <View style={findRiderStyle.findRiderMainView}>
+
+          <View style={findRiderStyle.postRideAvailableMembersContainer}>
+            <Text
+              style={findRiderStyle.findRiderLabel}
+            >Ride available from:</Text>
+
+            <ButtonGroup
+              onPress={(selectedindex) => {
+                setFindRiderState({
+                  ...findRiderState,
+                  selectedRouteIndex: selectedindex,
+                });
+              }}
+              selectedIndex={findRiderState.selectedRouteIndex}
+              buttons={config.ROUTE_INFO}
+            />
+          </View>
+
+
         </View>
       }
     </>
   );
 }
+
+const findRiderStyle = StyleSheet.create({
+  findRiderMainView: {
+    flexDirection: "column",
+  },
+  postRideAvailableMembersContainer: {
+    marginTop: 15,
+  },
+  findRiderLabel: {
+    fontWeight: "bold",
+  },
+});
+
+export default FindRider;
